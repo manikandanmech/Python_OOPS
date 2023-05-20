3d906f72da2f5cfc62b0d26d143977392b23461c
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-05-20 18:29:00 +0530

    Add files via upload

---
+++ ENCAPSULATION.py
@@ -0,0 +1,15 @@
+"""
+Encapsulation===>
+      Encapsulation is one of the critical features of object-oriented programming,
+      Encapsulation in Python describes the concept of bundling data and methods within a single unit.
+  3 kinds of access specifiers:
+   *Public Members
+   *Private Members
+   *Protected Members
+
+ """
+
+
+
+
+                   < I Wil update here quickly >
